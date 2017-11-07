8047d2c853cff7618d0b093776605b500deee49b
Erland <912859459@qq.com>
2017-11-07 10:30:02 +0800

    Update 八数码难题.cpp

--- Heuristically Search/八数码难题.cpp
+++ Heuristically Search/八数码难题.cpp
@@ -6,3 +6,124 @@
 *输出：只有一行，该行只有一个数字，表示从初始状态到目标状态需要的最少移动次数(测试数据中无特殊无法到达目标状态数据)
 */
 
+// BFS + Hash
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+#define N 500000
+struct node{int mp[4][4];}a[N];
+int g[4][4] = {						//目标数组
+	{ 0, 0, 0, 0 },
+	{ 0, 1, 2, 3 },
+	{ 0, 8, 0, 4 },
+	{ 0, 7, 6, 5 }
+};
+int xx[4] = { 0, 0, 1, -1 };		//四个方向移动
+int yy[4] = { 1, -1, 0, 0 };
+int hashArr[3733800] = {0};
+int step[N] = {0};
+int h = 0;			//当前状态
+int t = 1;			//总数
+bool flag = 0;		//结束标志
+
+
+int check()			//是否到达目标
+{
+	for (int i = 1; i <= 3; i++)
+	{
+		for (int j = 1; j <= 3; j++)
+		{
+			if (a[t].mp[i][j] != g[i][j])
+				return 0;
+		}
+	}
+	return 1;
+}
+
+int Hash()				//去重
+{
+	int s = 0, k = 1;
+	for (int i = 1; i <= 3; i++)
+	{
+		for (int j = 1; j <= 3; j++)
+			s += a[t].mp[i][j] * k, k *= 7;
+	}
+	s %= 3733799;
+	if (!hashArr[s])		//第一次出现
+	{
+		hashArr[s] = 1;
+		return 1; 
+	}
+	return 0;
+}
+
+int isOut(int x, int y)		//是否越界
+{
+	if (x >= 1 && x <= 3 && y >= 1 && y <= 3) 
+		return 1;
+	return 0;
+}
+
+void move(int x, int y)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		int p = x + xx[i], q = y + yy[i];
+		if (isOut(p, q))
+		{
+			for (int j = 1; j <= 3; j++)
+			{
+				for (int k = 1; k <= 3; k++)
+					a[t].mp[j][k] = a[h].mp[j][k];
+			}
+			swap(a[t].mp[x][y], a[t].mp[p][q]);
+			step[t] = step[h] + 1;
+			if (check())    
+			{
+				cout << step[t];
+				flag = 1; 
+				return; 
+			}
+			if (Hash())
+				t++;
+		}
+	}
+}
+
+void search()
+{
+	while (h < t)
+	{
+		for (int i = 1; i <= 3; i++)
+		{
+			for (int j = 1; j <= 3; j++)
+			{
+				if (a[h].mp[i][j] == 0)
+					move(i, j);
+				if (flag)
+					return;
+			}
+		}
+		h++;
+	}
+}
+
+void input()
+{
+	string str;
+	cin >> str;
+	for (int i = 1; i <= 3; i++)
+	{
+		for (int j = 1; j <= 3; j++)
+			a[0].mp[i][j] = str[(i - 1) * 3 + j - 1] - '0';		//初始状态
+	}
+}
+
+int main()
+{
+	input();
+	search();
+	return 0;
+}
