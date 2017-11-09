c89ca11e752c22719472e05eaf440c1be0233617
Erland <912859459@qq.com>
2017-11-09 20:59:05 +0800

    Update 线段树练习2.cpp

--- Segment Tree/线段树练习2.cpp
+++ Segment Tree/线段树练习2.cpp
@@ -39,3 +39,79 @@ int main()
 }
 
 
+//线段树解法
+
+#include <cstdio>
+using namespace std;
+#define min(a, b) (a < b ? a : b)
+const int SZ = 100000;
+struct SegTree{ int value; }seg[3 * SZ];
+int N, M, data[SZ];
+
+void build(int root, int left, int right)
+{
+	if (left == right)
+		seg[root].value = data[left];
+	else{
+		int mid = (left + right) / 2;
+		build(root * 2 + 1, left, mid);
+		build(root * 2 + 2, mid + 1, right);
+		seg[root].value = seg[root * 2 + 1].value + seg[root * 2 + 2].value;	//保存为左右结点的和
+	}
+}
+
+void update(int root, int left, int right, int index, int add)
+{
+	if (left == right)
+	{
+		if (index == left)
+			seg[root].value += add;
+		return;
+	}
+	int mid = (left + right) / 2;
+	if (index <= mid)
+		update(root * 2 + 1, left, mid, index, add);
+	else
+		update(root * 2 + 2, mid + 1, right, index, add);
+	seg[root].value = seg[root * 2 + 1].value + seg[root * 2 + 2].value;
+}
+
+int search(int root, int left, int right,int index)
+{
+	if (left == right)
+	{
+		if (index == left)
+			return seg[root].value;
+	}
+	int mid = (left + right) / 2;
+	if (index <= mid)
+		search(root * 2 + 1, left, mid, index);
+	else
+		search(root * 2 + 2, mid + 1, right, index);
+}
+
+int main()
+{
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &data[i]);
+	build(0, 0, N - 1);					//创建线段树,求和树
+	scanf("%d", &M);
+	int mode, a, b, c;
+	for (int i = 0; i < M; i++)
+	{
+		scanf("%d", &mode);
+		if (mode == 1)					//更新模式
+		{
+			scanf("%d %d %d", &a, &b, &c);
+			for (int j = a - 1; j <= b - 1; j++)
+				update(0, 0, N - 1, j, c);
+		}
+		else if (mode == 2)				//查询模式
+		{
+			scanf("%d", &a);
+			printf("%d\n", search(0, 0, N - 1, a - 1));
+		}
+	}
+	return 0;
+}
